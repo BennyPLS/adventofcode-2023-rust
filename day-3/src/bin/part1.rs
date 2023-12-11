7456c8784f408ae9efd720d5ed067956e1dc2a2c
Benny <peptoni0345@gmail.com>
2023-12-11 15:33:59 +0100

    Day 3 Completed - Part 1 & 2
    Added sample.txt to get the test input for simplicity.

--- day-3/src/bin/part1.rs
+++ day-3/src/bin/part1.rs
@@ -1,1 +1,157 @@
-fn main() {}
+trait IsSymbol {
+    fn is_symbol(&self) -> bool;
+}
+
+impl IsSymbol for char {
+    fn is_symbol(&self) -> bool {
+        self.is_ascii_punctuation() && self != &'.'
+    }
+}
+
+fn create_grid(input: &str) -> Vec<Vec<char>> {
+    let mut grid = Vec::new();
+
+    for line in input.lines() {
+        grid.push(line.chars().collect());
+    }
+
+    grid
+}
+
+struct PartNumber {
+    number: i32,
+    length: usize,
+    x: usize,
+    y: usize,
+}
+
+impl PartNumber {
+    fn is_valid(&self, grid: &Vec<Vec<char>>) -> bool {
+        let y_start = if self.y == 0 { 0 } else { self.y - 1 };
+        let y_end = if self.y == grid.len() - 1 {
+            grid.len() - 1
+        } else {
+            self.y + 1
+        };
+
+        let range = y_start..=y_end;
+
+        for i in range {
+            if self.scan_row(i, grid) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn scan_row(&self, y: usize, grid: &Vec<Vec<char>>) -> bool {
+        let x_start = if self.x == 0 { 0 } else { self.x - 1 };
+        let x_end = if self.x + self.length >= grid[y].len() - 1 {
+            grid[y].len() - 1
+        } else {
+            self.x + self.length
+        };
+        let range = x_start..=x_end;
+
+        for i in range {
+            if grid[y][i].is_symbol() {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+#[test]
+fn part_number_is_valid_test() {
+    let grid = create_grid(include_str!("./input1.txt"));
+
+    let part_number_true = PartNumber {
+        number: 426,
+        length: 3,
+        x: 9,
+        y: 0,
+    };
+
+    let part_number_false = PartNumber {
+        number: 687,
+        length: 3,
+        x: 6,
+        y: 12,
+    };
+
+    assert_eq!(part_number_true.is_valid(&grid), true);
+    assert_eq!(part_number_false.is_valid(&grid), false);
+}
+fn scan_grid(grid: &Vec<Vec<char>>) -> Vec<PartNumber> {
+    let mut part_numbers = Vec::new();
+
+    for (y, row) in grid.iter().enumerate() {
+        let mut x = 0;
+        while x < row.len() {
+            let mut part_number = String::new();
+            for i in x..row.len() {
+                if !row[i].is_digit(10) {
+                    break;
+                }
+
+                part_number.push(row[i]);
+            }
+
+            if part_number.len() > 0 {
+                let part_number = PartNumber {
+                    number: part_number.parse::<i32>().unwrap(),
+                    length: part_number.len(),
+                    x,
+                    y,
+                };
+
+                x += part_number.length;
+                part_numbers.push(part_number);
+            } else {
+                x += 1;
+            }
+        }
+    }
+
+    part_numbers
+}
+
+#[test]
+fn scan_grid_test() {
+    let grid = create_grid(include_str!("sample.txt"));
+    let part_numbers = scan_grid(&grid);
+
+    assert_eq!(part_numbers.len(), 10);
+}
+
+fn sum_valid_part_numbers(part_numbers: &Vec<PartNumber>, grid: &Vec<Vec<char>>) -> i32 {
+    let mut sum = 0;
+
+    for part_number in part_numbers {
+        if part_number.is_valid(&grid) {
+            sum += part_number.number;
+        }
+    }
+
+    sum
+}
+
+#[test]
+fn sum_valid_part_numbers_test() {
+    let grid = create_grid(include_str!("sample.txt"));
+    let part_numbers = scan_grid(&grid);
+    let sum = sum_valid_part_numbers(&part_numbers, &grid);
+
+    assert_eq!(sum, 4361);
+}
+
+fn main() {
+    let grid = create_grid(include_str!("./input1.txt"));
+    let part_numbers = scan_grid(&grid);
+    let sum = sum_valid_part_numbers(&part_numbers, &grid);
+
+    println!("Sum: {}", sum);
+}
--- day-3/src/bin/part2.rs
+++ day-3/src/bin/part2.rs
@@ -1,1 +1,201 @@
-fn main() {}
+trait IsSymbol {
+    fn is_symbol(&self) -> bool;
+}
+
+impl IsSymbol for char {
+    fn is_symbol(&self) -> bool {
+        self.is_ascii_punctuation() && self != &'.'
+    }
+}
+
+type Grid = Vec<Vec<char>>;
+
+fn create_grid(input: &str) -> Grid {
+    let mut grid = Vec::new();
+
+    for line in input.lines() {
+        grid.push(line.chars().collect());
+    }
+
+    grid
+}
+
+#[derive(Debug)]
+struct GearRatio {
+    numbers: Option<(i32, i32)>,
+    x: usize,
+    y: usize,
+}
+
+impl GearRatio {
+    fn scan(&mut self, grid: &Grid) -> bool {
+        let y_start = if self.y == 0 { 0 } else { self.y - 1 };
+        let y_end = {
+            if self.y == grid.len() - 1 {
+                grid.len() - 1
+            } else {
+                self.y + 1
+            }
+        };
+
+        let x_start = if self.x == 0 { 0 } else { self.x - 1 };
+        let x_end = {
+            if self.x == grid[0].len() - 1 {
+                grid[0].len() - 1
+            } else {
+                self.x + 1
+            }
+        };
+
+        let range = y_start..=y_end;
+        let mut numbers: Vec<i32> = Vec::new();
+
+        for y in range {
+            let row = &grid[y];
+            let mut x = x_start;
+            while x <= x_end {
+                if row[x].is_digit(10) {
+                    if let Some((number, last_index)) = get_number(row, x) {
+                        numbers.push(number);
+                        x = last_index;
+                    }
+                }
+
+                x += 1;
+            }
+        }
+
+        if numbers.len() != 2 {
+            return false;
+        }
+
+        self.numbers = Some((numbers[0], numbers[1]));
+        return true;
+    }
+
+    fn get_ratio(&self) -> Option<i32> {
+        if let Some((a, b)) = self.numbers {
+            Some(a * b)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn scan_test() {
+    let mut gear_ratio = GearRatio {
+        numbers: None,
+        x: 3,
+        y: 1,
+    };
+
+    let grid = create_grid(include_str!("./sample.txt"));
+
+    assert_eq!(gear_ratio.scan(&grid), true);
+
+    if let Some((a, b)) = gear_ratio.numbers {
+        assert_eq!(a, 467);
+        assert_eq!(b, 35);
+    }
+
+    gear_ratio = GearRatio {
+        numbers: None,
+        x: 5,
+        y: 8,
+    };
+
+    assert_eq!(gear_ratio.scan(&grid), true);
+    if let Some((a, b)) = gear_ratio.numbers {
+        assert_eq!(a, 755);
+        assert_eq!(b, 598);
+    }
+}
+
+fn get_number(row: &Vec<char>, x: usize) -> Option<(i32, usize)> {
+    let mut part_number = String::new();
+    let mut last_digit_index = 0;
+    let mut x = x;
+
+    if !row[x].is_digit(10) {
+        return None;
+    }
+
+    while x > 0 && row.get(x - 1).is_some() && row[x - 1].is_digit(10) {
+        x -= 1;
+    }
+
+    for i in x..row.len() {
+        if !row[i].is_digit(10) {
+            break;
+        }
+
+        part_number.push(row[i]);
+
+        last_digit_index = i;
+    }
+
+    Some((part_number.parse::<i32>().unwrap(), last_digit_index))
+}
+
+#[test]
+fn get_number_test() {
+    let row = vec!['1', 'a', '3', '4', 'a', '6', '7', 'a', '9'];
+    assert_eq!(get_number(&row, 0), Some((1, 0)));
+    assert_eq!(get_number(&row, 2), Some((34, 3)));
+    assert_eq!(get_number(&row, 3), Some((34, 3)));
+    assert_eq!(get_number(&row, 5), Some((67, 6)));
+    assert_eq!(get_number(&row, 7), None);
+    assert_eq!(get_number(&row, 8), Some((9, 8)));
+}
+
+fn get_gears(grid: &Grid) -> Vec<GearRatio> {
+    let mut gears: Vec<GearRatio> = Vec::new();
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, char) in row.iter().enumerate() {
+            if char.is_symbol() {
+                let mut gear = GearRatio {
+                    numbers: None,
+                    x,
+                    y,
+                };
+
+                if gear.scan(&grid) {
+                    gears.push(gear);
+                }
+            }
+        }
+    }
+
+    gears
+}
+
+fn sum_gear_ratios(gears: &Vec<GearRatio>) -> i32 {
+    let mut sum = 0;
+
+    for gear in gears {
+        if let Some(ratio) = gear.get_ratio() {
+            sum += ratio;
+        }
+    }
+
+    sum
+}
+
+#[test]
+fn sum_gear_ratios_test() {
+    let grid = create_grid(include_str!("./sample.txt"));
+    let gears = get_gears(&grid);
+    let sum = sum_gear_ratios(&gears);
+
+    assert_eq!(sum, 467835);
+}
+
+fn main() {
+    let grid = create_grid(include_str!("./input2.txt"));
+    let gears = get_gears(&grid);
+    let sum = sum_gear_ratios(&gears);
+
+    println!("Sum of gear ratios: {}", sum);
+}
